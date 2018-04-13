785d7e5860067dafe85ec527e55903d660850d2d
Molyktech <modupeadebayo001@gmail.com>
2018-04-13 15:32:05 -0700

    Modupe's commit

---
+++ time.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en US">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="time.css">
+        <link href='https://fonts.googleapis.com/css?family=Rhodium Libre' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet'>
+       
+        <title>Current Time</title>
+    </head>
+    <body>
+        <section>
+                <h1 class="head">Hotels.ng</h1>
+                <div class="time">
+                    <h3>Don't watch the clock, do <br />what it does: Keep Going!</h3>
+                    <h1 id="php-time">  
+                         <?php echo date('h:i:s A');?>   
+                    </h1>
+                </div>
+              
+                   
+               
+            <div class="container">
+               
+                <div class="img1"></div>
+                <div class="img2"></div>
+                <div class="img3"></div>
+            </div>
+         
+        </section>
+        
+    </body>
+</html>
